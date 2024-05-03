9e441d5ebf924b6ff6850226923f8fe346d7de50
Adriano Ferrari <irarrefonairda@gmail.com>
2024-05-03 20:27:20 -0300

    Posição 10994

--- educa/urls.py
+++ educa/urls.py
@@ -1,7 +1,8 @@
-"""educa URL Configuration
+"""
+URL configuration for educa project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
